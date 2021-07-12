8daf1eb1b7ce42545de91c3e1cb2c04534229c6c
christina <chrstnaheadley@gmail.com>
2021-07-12 18:26:30 -0400

    add reset passwords

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -12,4 +12,25 @@ class PasswordResetsController < ApplicationController
 
     redirect_to root_path, notice: "Password reset email sent if account exists for the email provided."
   end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "Link expired. Please try again."
+  end
+  
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Your password has been updated."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 end
