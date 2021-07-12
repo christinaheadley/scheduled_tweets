c08049c6326d697885deff0e2d997fe4df05e5eb
christina <chrstnaheadley@gmail.com>
2021-07-12 14:31:15 -0400

    adds mailer

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,11 @@ Rails.application.routes.draw do
   get "/sign_in", to: "sessions#new"
   post "/sign_in", to: "sessions#create"
 
+  get "/password/reset", to: "password_resets#new"
+  post "/password/reset", to: "password_resets#create"
+  get "/password/reset/edit", to: "password_resets#edit"
+  patch "/password/reset/edit", to: "password_resets#update"
+
   delete "/logout", to: "sessions#destroy"
 
   # EXAMPLE HTML ROUTE
